5139ad1b2dc826888b5737004670f09323ed069b
PraveenJeendru <wasadmin007@gmail.com>
2016-06-11 19:22:40 -0400

    Httpd Configuration

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://dev-jindru/');
+define('IMAGE_SERVER', 'http://dev-jindru/image');
+// HTTPS
+define('HTTPS_SERVER', 'http://dev-jindru/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/jindru.com/catalog/');
+define('DIR_SYSTEM', '/var/www/html/jindru.com/system/');
+define('DIR_DATABASE', '/var/www/html/jindru.com/system/database/');
+define('DIR_LANGUAGE', '/var/www/html/jindru.com/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/jindru.com/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/jindru.com/system/config/');
+define('DIR_IMAGE', '/var/www/html/jindru.com/image/');
+define('DIR_CACHE', '/var/www/html/jindru.com/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/jindru.com/download/');
+define('DIR_LOGS', '/var/www/html/jindru.com/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'wasadmin_ocdev');
+define('DB_PREFIX', 'oc_');
+?>
